6f579c4f8075c0191c7bf7b439b89a4e3e5f7163
Serggi0 <serve4mia@gmail.com>
2020-11-28 20:46:48 +0300

    Add brain-even module (even-cli.py and brain-even.py)

---
+++ brain_games/even_cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import prompt
+from random import randint
+
+
+def check_even():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name, '!', sep='')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    for i in range(3):
+        random_number = randint(0, 101)
+        print('Question: ', random_number)
+        answer = prompt.string('Your answer: ')
+        if i == 2:
+            print('Congratulations, ', name, '!', sep='')
+        else:
+            if random_number % 2 == 0:
+                if answer == 'yes':
+                    print('Correct!')
+                    i += 1
+                elif answer == 'no':    
+                    print("'no' is wrong answer ;(. Correct answer was 'yes'.")
+                    print("Let's try again, ", name)
+                    break
+                else:
+                    print(answer, " is wrong answer ;(. Correct answer was 'yes'.")
+                    break
+            else:
+                if answer == 'no':
+                    print('Correct!')
+                    i += 1
+                elif answer == 'yes':    
+                    print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+                    print("Let's try again, ", name)
+                    break
+                else:
+                    print(answer, " is wrong answer ;(. Correct answer was 'no'.")
+                    break
+
+
+        
---
+++ brain_games/scripts/brain-even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import brain_games.even_cli
+    
+
+def main():
+    brain_games.even_cli.check_even()
+
+
+if __name__ == '__main__':
+    main()
