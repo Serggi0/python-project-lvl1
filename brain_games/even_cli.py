baa03c7e49645bd3933d0346a46e5ca9c5559f61
Serggi0 <serve4mia@gmail.com>
2020-11-29 17:39:34 +0300

    Modify even_cli.py

--- brain_games/even_cli.py
+++ brain_games/even_cli.py
@@ -4,27 +4,26 @@ from random import randint
 
 
 def check_even():
+    i = 0
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, ', name, '!', sep='')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    for i in range(3):
+    while i < 3:
         random_number = randint(0, 101)
         print('Question: ', random_number)
         answer = prompt.string('Your answer: ')
-        if i <= 2:
+        if i < 3:
             if random_number % 2 == 0:
                 if answer == 'yes':
                     print('Correct!')
                     i += 1
                 elif answer == 'no':
-                    i == 0
                     print("'no' is wrong answer ;(.\
  Correct answer was 'yes'.")
                     print("Let's try again, ", name)
                     break
                 else:
-                    i == 0
                     print("'", answer, "'", " is wrong answer ;(.\
  Correct answer was 'yes'.", sep='')
                     break
@@ -33,15 +32,13 @@ def check_even():
                     print('Correct!')
                     i += 1
                 elif answer == 'yes':
-                    i == 0
                     print("'yes' is wrong answer ;(.\
  Correct answer was 'no'.")
                     print("Let's try again, ", name)
                     break
                 else:
-                    i == 0
                     print("'", answer, "'", " is wrong answer ;(.\
  Correct answer was 'yes'.", sep='')
                     break
-        if i == 2:
-            print('Congratulations, ', name, '!', sep='')
+    if i == 3:
+        print('Congratulations, ', name, '!', sep='')
