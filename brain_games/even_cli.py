8cb8eed33383c4914974c0061fc29aca4c809929
Serggi0 <serve4mia@gmail.com>
2020-11-29 16:51:44 +0300

    Modify even_cli.py

--- brain_games/even_cli.py
+++ brain_games/even_cli.py
@@ -12,32 +12,36 @@ def check_even():
         random_number = randint(0, 101)
         print('Question: ', random_number)
         answer = prompt.string('Your answer: ')
-        if i == 2:
-            print('Congratulations, ', name, '!', sep='')
-        else:
+        if i <= 2:
             if random_number % 2 == 0:
                 if answer == 'yes':
                     print('Correct!')
                     i += 1
                 elif answer == 'no':
+                    i == 0
                     print("'no' is wrong answer ;(.\
-                    Correct answer was 'yes'.")
+ Correct answer was 'yes'.")
                     print("Let's try again, ", name)
                     break
                 else:
-                    print(answer, " is wrong answer ;(.\
-                    Correct answer was 'yes'.")
+                    i == 0
+                    print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was 'yes'.", sep='')
                     break
             else:
                 if answer == 'no':
                     print('Correct!')
                     i += 1
                 elif answer == 'yes':
+                    i == 0
                     print("'yes' is wrong answer ;(.\
-                    Correct answer was 'no'.")
+ Correct answer was 'no'.")
                     print("Let's try again, ", name)
                     break
                 else:
-                    print(answer, " is wrong answer ;(.\
-                    Correct answer was 'no'.")
+                    i == 0
+                    print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was 'yes'.", sep='')
                     break
+        if i == 2:
+            print('Congratulations, ', name, '!', sep='')
