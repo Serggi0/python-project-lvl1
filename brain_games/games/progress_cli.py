3a9d4b3e8a2b65326916593b85689eb404b8197f
Serggi0 <serve4mia@gmail.com>
2020-12-02 22:24:59 +0300

    Add brain_progression.py and progress_cli.py for progression game

---
+++ brain_games/games/progress_cli.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import prompt
+
+
+def hello():
+    print('Welcome to the Brain Games!')
+    global name
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name, '!', sep='')
+
+
+def invite_game():
+    print('What number is missing in the progression?')
+
+
+def result_play():
+    from random import randint, choice
+    count_member_prgr = 10
+    list_step_prgr = [i for i in range(-10, 11, 1)]
+    list_step_prgr.remove(0)
+    step_prgr = choice(list_step_prgr)
+    start_prgr = randint(1, 99)
+    finish_prgr = start_prgr + count_member_prgr * step_prgr
+    list_prgr = [i for i in range(start_prgr, finish_prgr, step_prgr)]
+    random_index = randint(1, 9)
+    check_play = list_prgr[random_index]
+    list_prgr[random_index] = '..'
+    print('Question: ', list_prgr)
+    return check_play
+
+
+def round_game_1():
+    check_play = result_play()
+    answer = prompt.string('Your answer: ')
+    if answer == str(check_play):
+        print('Correct!')
+        return True
+    else:
+        print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was ", "'", check_play, "'", sep='')
+        print("Let's try again, ", name)
+        return False
+
+
+def round_game_2():
+    check_play = result_play()
+    answer = prompt.string('Your answer: ')
+    if answer == str(check_play):
+        print('Correct!')
+        return True
+    else:
+        print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was ", "'", check_play, "'", sep='')
+        print("Let's try again, ", name)
+        return False
+
+
+def round_game_3():
+    check_play = result_play()
+    answer = prompt.string('Your answer: ')
+    if answer == str(check_play):
+        print('Correct!')
+        print('Congratulations, ', name, '!', sep='')
+        return True
+    else:
+        print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was ", "'", check_play, "'", sep='')
+        print("Let's try again, ", name)
+        return False
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import brain_games.games.progress_cli
+
+
+def main():
+    brain_games.games.progress_cli.hello()
+    brain_games.games.progress_cli.invite_game()
+    if brain_games.games.progress_cli.round_game_1() is True:
+        if brain_games.games.progress_cli.round_game_2() is True:
+            brain_games.games.progress_cli.round_game_3()
+
+
+if __name__ == '__main__':
+    main()
