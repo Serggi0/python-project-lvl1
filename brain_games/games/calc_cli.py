285e1f3388415e8eec5737df738abc903d23c2dc
Serggi0 <serve4mia@gmail.com>
2020-12-02 13:59:47 +0300

    Modify calc_cli and brain-calc in OOP logic

--- brain_games/games/calc_cli.py
+++ brain_games/games/calc_cli.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 import prompt
-from random import randint, choice
-
-
-list_operator = ['+', '-', '*']
-count_rounds = 3
 
 
 def hello():
@@ -13,29 +8,62 @@ def hello():
     name = prompt.string('May I have your name? ')
     print('Hello, ', name, '!', sep='')
 
-
-def calc_game():
-    i = 0
+def invite_game():
     print('What is the result of the expression?')
-    while i < count_rounds:
-        random_number1 = randint(1, 101)
-        random_number2 = randint(1, 10)
-        random_operator = choice(list_operator)
-        if random_operator == '+':
-            expression = random_number1 + random_number2
-        elif random_operator == '-':
-            expression = random_number1 - random_number2
-        elif random_operator == '*':
-            expression = random_number1 * random_number2
-        print('Question: ', random_number1, random_operator, random_number2)
-        answer = prompt.string('Your answer: ')
-        if answer == str(expression):
-            print('Correct!')
-            i += 1
-        else:
-            print("'", answer, "'", " is wrong answer ;(.\
- Correct answer was ", "'", expression, "'", sep='')
-            print("Let's try again, ", name)
-            break
-    if i == count_rounds:
+
+def result_play():
+    from random import randint, choice
+    list_operator = ['+', '-', '*']
+    random_number1 = randint(1, 101)
+    random_number2 = randint(1, 10)
+    random_operator = choice(list_operator)
+    if random_operator == '+':
+        expression = random_number1 + random_number2
+    elif random_operator == '-':
+        expression = random_number1 - random_number2
+    elif random_operator == '*':
+        expression = random_number1 * random_number2
+    print('Question: ', random_number1, random_operator, random_number2)
+    return expression
+
+
+def round_game_1():
+    check_play = result_play()
+    answer = prompt.string('Your answer: ')
+    if answer == str(check_play):
+        print('Correct!')
+        return True
+    else:
+        print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was ", "'", check_play, "'", sep='')
+        print("Let's try again, ", name)
+        return False
+
+
+def round_game_2():
+    check_play = result_play()
+    answer = prompt.string('Your answer: ')
+    if answer == str(check_play):
+        print('Correct!')
+        return True
+    else:
+        print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was ", "'", check_play, "'", sep='')
+        print("Let's try again, ", name)
+        return False
+
+
+def round_game_3():
+    check_play = result_play()
+    answer = prompt.string('Your answer: ')
+    if answer == str(check_play):
+        print('Correct!')
         print('Congratulations, ', name, '!', sep='')
+        return True
+    else:
+        print("'", answer, "'", " is wrong answer ;(.\
+ Correct answer was ", "'", check_play, "'", sep='')
+        print("Let's try again, ", name)
+        return False
+
+
--- brain_games/scripts/brain-calc.py
+++ brain_games/scripts/brain-calc.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-
 import brain_games.games.calc_cli
 
 
 def main():
     brain_games.games.calc_cli.hello()
-    brain_games.games.calc_cli.calc_game()
+    brain_games.games.calc_cli.invite_game()
+    if brain_games.games.calc_cli.round_game_1() == True:
+        if brain_games.games.calc_cli.round_game_2() == True:
+            brain_games.games.calc_cli.round_game_3()
 
 
 if __name__ == '__main__':
