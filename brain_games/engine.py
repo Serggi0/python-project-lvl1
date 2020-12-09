d472cc8e37c2573847b74c87cbccc1d061cbecae
Serggi0 <serve4mia@gmail.com>
2020-12-09 18:51:29 +0300

    Modify engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -15,16 +15,16 @@ def run_game(cli):
     count_correct_answer = 0
     hello(cli)
     while count_correct_answer < count_rounds:
-        random_number, correct_answer = cli.round()
-        print('Question: ', random_number)
+        round_question, correct_answer = cli.round()
+        print('Question: ', round_question)
         user_answer = prompt.string('Your answer: ')
         if user_answer == correct_answer:
             count_correct_answer += 1
             print('Correct!')
         else:
             print("'", user_answer, "'", " is wrong answer ;(.\
- Correct answer was ", "'", correct_answer, "'", sep='')
-            print("Let's try again,", name)
+ Correct answer was ", "'", correct_answer, "'", ".", sep='')
+            print("Let's try again, ", name, "!", sep='')
             break
     if count_correct_answer == count_rounds:
         print('Congratulations, ', name, '!', sep='')
