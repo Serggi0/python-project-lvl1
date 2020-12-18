2decce1ad755b13faa9a03a7a8f6522f70507546
Serggi0 <serve4mia@gmail.com>
2020-12-18 17:41:10 +0300

    Modify engine.py: join function, add print template

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,28 +3,23 @@ import prompt
 count_rounds = 3
 
 
-def hello(cli):
+def run(game):
     print('Welcome to the Brain Games!')
-    global name
     name = prompt.string('May I have your name? ')
-    print('Hello, ', name, '!', sep='')
-    print(cli.condition)
-
-
-def run_game(cli):
+    print("Hello, {}!".format(name))
+    print(game.DESCRIPTION)
     count_correct_answer = 0
-    hello(cli)
     while count_correct_answer < count_rounds:
-        round_question, correct_answer = cli.round()
-        print('Question:', round_question)
+        round_question, correct_answer = game.round()
+        print("Question: {}".format(round_question))
         user_answer = prompt.string('Your answer: ')
         if user_answer == correct_answer:
             count_correct_answer += 1
             print('Correct!')
         else:
-            print("'", user_answer, "'", " is wrong answer ;(.\
- Correct answer was ", "'", correct_answer, "'", ".", sep='')
-            print("Let's try again, ", name, "!", sep='')
+            print("'{}' is wrong answer ;(.\
+ Correct answer was '{}'.".format(user_answer, correct_answer))
+            print("Let's try again, {}!".format(name))
             break
     if count_correct_answer == count_rounds:
-        print('Congratulations, ', name, '!', sep='')
+        print("Congratulations, {}".format(name))
